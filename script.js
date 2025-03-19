842d63783cb469c84b0ca7601df1922953f07f55
juancarlosayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-03-19 08:57:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registered:", registration);
-      })
-      .catch((error) => {
-        console.log("Service Worker registration failed:", error);
-      });
-  });
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/homepage/service-worker.js")
+            .then((registration) => {
+                console.log("Service Worker registered:", registration);
+            })
+            .catch((error) => {
+                console.log("Service Worker registration failed:", error);
+            });
+    });
 }
